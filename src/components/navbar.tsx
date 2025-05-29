1c9d8053ff39b5bdde239c0d2a68e8f32de4c3d0
justin-0 <itsjustinoconnell@gmail.com>
2025-05-29 14:22:19 +0100

    shadcn dropdown component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,12 @@ import { Moon } from "lucide-react";
 import { Link } from "react-router";
 import { Avatar } from "./ui/avatar";
 import { AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Navbar() {
   return (
@@ -12,10 +18,20 @@ export default function Navbar() {
       <div className="flex items-center gap-4">
         <Link to="/dashboard">Dashboard</Link>
         <Moon />
-        <Avatar>
-          <AvatarImage src="https://avatars.githubusercontent.com/u/203975120?v=4" />
-          <AvatarFallback>JOC</AvatarFallback>
-        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage
+                src="https://avatars.githubusercontent.com/u/203975120?v=4"
+                className="cursor-pointer"
+              />
+              <AvatarFallback>JOC</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );

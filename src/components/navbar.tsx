f54a0f5d60bac6cdee61c76cc6e69d8a83c7202e
justin-0 <itsjustinoconnell@gmail.com>
2025-05-29 14:14:51 +0100

    shadcn avatar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,22 @@
+import { Moon } from "lucide-react";
+import { Link } from "react-router";
+import { Avatar } from "./ui/avatar";
+import { AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <nav className="p-4 flex items-center justify-between">
+      {/* Left */}
+      collapseButton
+      {/* Right */}
+      <div className="flex items-center gap-4">
+        <Link to="/dashboard">Dashboard</Link>
+        <Moon />
+        <Avatar>
+          <AvatarImage src="https://avatars.githubusercontent.com/u/203975120?v=4" />
+          <AvatarFallback>JOC</AvatarFallback>
+        </Avatar>
+      </div>
+    </nav>
+  );
 }
